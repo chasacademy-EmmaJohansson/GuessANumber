4592c89b691b7d749f09b7ee58edea9be03da7bb
chasacademy-EmmaJohansson <114146126+chasacademy-EmmaJohansson@users.noreply.github.com>
2022-11-15 13:58:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector(".guesses");
 const prevResult = document.querySelector(".prevResult");
-const hidegreen = document.querySelector (".green");
-const hidered = document.querySelector (".red");
-const gameover = document.querySelector (".gameover");
 const lowHiMessage = document.querySelector(".lowHiMessage");
+const againButton = document.getElementById("againButton");
 
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
 
 let guessCount = 1;
 //håller koll på hur många gissningar anv har haft genom att lagra gissningsantal på 1.
-let resetButton;
+
+
+  
+
 
 function checkGuess() {
   let userGuess = Number(guessField.value);
@@ -23,20 +24,32 @@ function checkGuess() {
   guesses.textContent += userGuess + " ";
   
   if (userGuess === randomNumber) {
-  prevResult.textContent = "Congratulations! You got it right!";
-  hidegreen.classList.remove ("green")
+  prevResult.innerHTML= "Congratulations! You got it right!";
+  prevResult.style.color = "white";
+  prevResult.style.backgroundColor = "green";
   lowHiMessage.textContent = "";
+
   } else if (guessCount === 5) {
-  gameover.classList.remove ("gameover");
+    prevResult.innerHTML ="Game over!"
+    prevResult.style.color ="white";
+    prevResult.style.backgroundColor = "purple";
+    setTimeout(function (){
+      window.location.reload();
+    }, 2000);
+
   } else {
-  prevResult.textContent = "Wrong!";
-  hidered.classList.remove ("red")
+  prevResult.innerHTML = "Wrong, try again!";
+  prevResult.style.color = "black";
+  prevResult.style.backgroundColor = "red";
+
+
   //För att vägleda anv skriver vi ut om input värdet är för högt ellet för lågt
   if(userGuess < randomNumber) {
   lowHiMessage.textContent = "Too low!";
   } else if(userGuess > randomNumber) {
   lowHiMessage.textContent = "Too high!";
   }
+ 
   }
   //Detta inför anv nästa gissning, vi lägger till 1 för att anv ska få en ny gissningschans
 guessCount++;
@@ -44,4 +57,11 @@ guessField.value = "";
 guessField.focus();
 }
 guessSubmit.addEventListener("click", checkGuess);
+
+//Nollställ och börja om
+const refreshPage = () => {
+  location.reload();
+}
+againButton.addEventListener("click", refreshPage);
+ 
 
